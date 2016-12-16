baccb150b0297b60bfc3c5aa6c2576401d9ea7b6
zhoumin <553419781@qq.com>
2016-12-16 17:54:16 +0800

    drawable

--- app/src/main/java/com/simpleway/min/view/DrawableActivity.java
+++ app/src/main/java/com/simpleway/min/view/DrawableActivity.java
@@ -1,7 +1,11 @@
 package com.simpleway.min.view;
 
 import android.annotation.SuppressLint;
+import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.ClipDrawable;
+import android.graphics.drawable.LevelListDrawable;
+import android.graphics.drawable.RotateDrawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -21,16 +25,57 @@ public class DrawableActivity extends AbsActionbarActivity {
 
     @InjectView(R.id.imageView6)
     ImageView imageView6;
+    @InjectView(R.id.imageView7)
+    ImageView imageView7;
+    @InjectView(R.id.imageView8)
+    ImageView imageView8;
+    @InjectView(R.id.imageView9)
+    ImageView imageView9;
+    @InjectView(R.id.imageView10)
+    ImageView imageView10;
 
-    private ClipDrawable cd;
+    private ClipDrawable clipDrawable;
+    private RotateDrawable rotateDrawable;
+    private AnimationDrawable animationDrawable;
+    private TransitionDrawable transitionDrawable;
+    private LevelListDrawable levelListDrawable;
+    private int count;
 
     @SuppressLint("handlerLeak")
     private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            if (msg.what == 0x123) {
-                cd.setLevel(cd.getLevel() + 500);
+            switch (msg.what) {
+                case 1:
+                    if (clipDrawable.getLevel() >= 10000) {
+                        clipDrawable.setLevel(0);
+                    } else {
+                        clipDrawable.setLevel(clipDrawable.getLevel() + 500);
+                    }
+                    break;
+                case 2:
+                    if (rotateDrawable.getLevel() >= 10000) {
+                        rotateDrawable.setLevel(0);
+                    } else {
+                        rotateDrawable.setLevel(rotateDrawable.getLevel() + 400);
+                    }
+                    break;
+                case 4:
+                    if (count % 2 == 0) {
+                        transitionDrawable.startTransition(2000);
+                    } else {
+                        transitionDrawable.reverseTransition(2000);
+                    }
+                    break;
+                case 5:
+                    if (levelListDrawable.getLevel() > 10000) {
+                        levelListDrawable.setLevel(0);
+                    }
+                    imageView10.setImageLevel(levelListDrawable.getLevel() + 2000);
+                    break;
             }
+
+            transitionDrawable = (TransitionDrawable) imageView9.getDrawable();
         }
     };
 
@@ -39,33 +84,63 @@ public class DrawableActivity extends AbsActionbarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawable);
         ButterKnife.inject(this);
-        clipPic();
+        go(1);
     }
 
 
-    @OnClick({R.id.imageView4, R.id.imageView6})
+    @OnClick({R.id.imageView4, R.id.imageView6, R.id.imageView7, R.id.imageView8, R.id.imageView9, R.id.imageView10})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.imageView4:
                 StartActivity(ViewActivity.class);
                 break;
             case R.id.imageView6:
-                clipPic();
+                go(1);
+                break;
+            case R.id.imageView7:
+                go(2);
+                break;
+            case R.id.imageView8:
+                go(3);
+                break;
+            case R.id.imageView9:
+                count++;
+                go(4);
+                break;
+            case R.id.imageView10:
+                go(5);
                 break;
         }
     }
 
 
-    private void clipPic() {
-        // 核心实现代码
-        cd = (ClipDrawable) imageView6.getDrawable();
-        final Timer timer = new Timer();
+    private void go(final int what) {
+        switch (what) {
+            case 1:
+                clipDrawable = (ClipDrawable) imageView6.getDrawable();
+                break;
+            case 2:
+                rotateDrawable = (RotateDrawable) imageView7.getDrawable();
+                break;
+            case 3:
+                animationDrawable = (AnimationDrawable) imageView8.getDrawable();
+                break;
+            case 4:
+                transitionDrawable = (TransitionDrawable) imageView9.getDrawable();
+                break;
+            case 5:
+                levelListDrawable = (LevelListDrawable) imageView10.getDrawable();
+                break;
+        }
+
+        Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                handler.sendEmptyMessage(0x123);
-                if (cd.getLevel() >= 10000) {
-                    timer.cancel();
+                if (what == 3) {
+                    animationDrawable.start();
+                } else {
+                    handler.sendEmptyMessage(what);
                 }
             }
         }, 0, 300);
